50c4ece8852d0d0c6ce8249f8f8ff1f7fe0f4e12
Code-Crafting <monojitsen90@gmail.com>
2025-09-05 14:36:44 +0530

    add responsiveness to shrimmer

--- src/components/Shrimmer/HeroShrimmer.jsx
+++ src/components/Shrimmer/HeroShrimmer.jsx
@@ -1,12 +1,12 @@
 function HeroShrimmer() {
   return (
-    <div className="max-w-7xl bg-overview h-[720px]">
-      <div className="w-1/2 absolute bottom-48 left-24">
-        <div className="bg-info max-w-lg h-[48px]"></div>
-        <div className="mt-4 bg-info max-w-3xs h-[32px]"></div>
+    <div className="max-w-7xl bg-overview md:h-[700px] 512px:h-[500px] 448px:h-[420px] h-[370px]">
+      <div className="512px:w-1/2 w-[70%] absolute md:bottom-48 md:left-24 448px:bottom-32 448px:left-12 bottom-24 left-8">
+        <div className="bg-info max-w-lg md:h-[48px] h-[36px]"></div>
+        <div className="md:mt-4 mt-2 bg-info max-w-3xs md:h-[32px] h-[20px]"></div>
         <div className="flex gap-4 mt-8">
-          <div className="bg-info flex items-center gap-2 w-[112px] h-[48px] justify-center rounded-sm hover:cursor-pointer"></div>
-          <div className="bg-info flex items-center gap-2 w-[150px] h-[48px] justify-center rounded-sm hover:cursor-pointer"></div>
+          <div className="bg-info flex items-center gap-2 md:w-[112px] md:h-[48px] w-[96px] h-[36px] justify-center rounded-sm hover:cursor-pointer"></div>
+          <div className="bg-info flex items-center gap-2 md:w-[150px] w-[125px] md:h-[48px] h-[36px] justify-center rounded-sm hover:cursor-pointer"></div>
         </div>
       </div>
     </div>
--- src/components/Shrimmer/PlayerShrimmer.jsx
+++ src/components/Shrimmer/PlayerShrimmer.jsx
@@ -1,15 +1,15 @@
 function PlayerShrimmer() {
   return (
-    <div className="w-1/2 flex flex-col gap-4 justify-center">
-      <div className="bg-info max-w-lg h-[48px]"></div>
-      <div className="bg-info max-w-sm h-[32px]"></div>
+    <div className="md:w-1/2 w-full flex flex-col gap-4 justify-center">
+      <div className="bg-info max-w-lg  md:h-[48px] h-[36px]"></div>
+      <div className="bg-info max-w-sm md:h-[32px] h-[24px]"></div>
       <div className="flex gap-4 items-center">
-        <div className="bg-info w-[125px] h-[32px]"></div>
-        <div className="bg-info w-[125px] h-[32px]"></div>
+        <div className="bg-info md:w-[125px] w-[96px] md:h-[32px] h-[24px]"></div>
+        <div className="bg-info md:w-[125px] w-[96px] md:h-[32px] h-[24px]"></div>
       </div>
       <div className="flex gap-4 items-center">
-        <div className="bg-info w-[125px] h-[32px]"></div>
-        <div className="bg-info w-[125px] h-[32px]"></div>
+        <div className="bg-info md:w-[125px] w-[96px] md:h-[32px] h-[24px]"></div>
+        <div className="bg-info md:w-[125px] w-[96px] md:h-[32px] h-[24px]"></div>
       </div>
     </div>
   );
